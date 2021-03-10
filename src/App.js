f295beed3e2251f3aa348a986674820d83cc3e1e
powerima <powerima@naver.com>
2021-03-10 23:38:36 +0900

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({name, picture}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} width="200"  />
+  </div>;
+}
+const foodILike = [
+  {
+  name: "Kimchi",
+  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+  name: "bulgogi",
+  image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/12/30/df939769792632a48a0eba8bc895e8601.jpg"
+  },
+  {
+  name: "kimbap",
+  image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/04/08/1d26c0444e724bca8ed271b24da0057a1.jpg"
+  },
+  {
+  name: "samgyetang",
+  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  }
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!</h1>
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
     </div>
-  );
+  )
 }
 
 export default App;
